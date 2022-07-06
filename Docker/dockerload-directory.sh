8ed5784d3d78d2f1d53f9f37f9c062d94fa8b028
WSBCRPT <>
2022-07-06 12:09:18 -0400

    Threw in a small docker loading script I used once

---
+++ Docker/dockerload-directory.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+echo "Grabbing current working directory..."
+wkdir=$PWD
+echo
+echo "current working directory is ${wkdir}"
+echo
+echo "dockerizing the tars!"
+echo "............."
+echo "whatever that means..."
+echo "loading first image, standby..."
+for filename in ${wkdir}/*.tar.gz ; do docker load -i "$filename" ; done
+exit
